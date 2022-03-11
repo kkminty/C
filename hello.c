c0fcc9e5c4bd5fdce7dbc5d9d6ec01195d40233e
qing <kkmintyq@gmail.com>
2022-03-11 21:28:20 +0100

    1

--- hello.c
+++ hello.c
@@ -4,21 +4,27 @@
 #include <conio.h>
 
  
-double factorial(unsigned int i)
-{
-   if(i <= 1)
-   {
-      return 1;
-   }
-   return i * factorial(i - 1);
-}
-int  main()
-{
-    int i = 5;
-    printf(" The factorial of %d is %f\n", i, factorial(i));
+long count;//长整型
+void move(int n,char A,char B,char C){//将n个圆盘从A立柱借助B立柱移动到C立柱上
 
-    system("pause");
-    return 0;
+if(n==1){//当只有一个圆盘时
+    printf("移动第%d次：%c->%c\n",++count,A,C);
+}else{
+    move(n-1,A,C,B);//注意立柱顺序变为A、C、B
+    printf("移动第%d次：%c->%c\n",++count,A,C);
+    move(n-1,B,A,C);
+    }
+}
+int main(){
+ //SetConsoleOutputCP(65001); 
+ count=0;//在主函数中将全局变量count初始化
+ int n;//圆盘总数
+ printf("请输入需要移动的总圆盘数：\n");
+ scanf("%d",&n);
+ move(n,'A','B','C');
+
+
+ system("pause");
 }
 
 
--- static.c
+++ static.c
@@ -72,3 +72,27 @@ int  main()
     system("pause");
     return 0;
 }
+
+/************************************************/
+/*函数递归实现汉纳塔*/
+/************************************************/
+long count;//长整型
+void move(int n,char A,char B,char C){//将n个圆盘从A立柱借助B立柱移动到C立柱上
+
+if(n==1){//当只有一个圆盘时
+    printf("移动第%d次：%c->%c\n",++count,A,C);
+}else{
+    move(n-1,A,C,B);//注意立柱顺序变为A、C、B
+    printf("移动第%d次：%c->%c\n",++count,A,C);
+    move(n-1,B,A,C);
+    }
+}
+int main(){
+ //SetConsoleOutputCP(65001); 
+ count=0;//在主函数中将全局变量count初始化
+ int n;//圆盘总数
+ printf("请输入需要移动的总圆盘数：\n");
+ scanf("%d",&n);
+ move(n,'A','B','C');
+ system("pause");
+}
