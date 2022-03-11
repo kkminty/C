e7b51d77b8515252f0f3dba3d0c954544bd89ea4
qing <kkmintyq@gmail.com>
2022-03-11 20:49:59 +0100

    1

--- hello.c
+++ hello.c
@@ -2,31 +2,23 @@
 #include <stdlib.h>
 #include <math.h>
 #include <conio.h>
-#define IN 1 /* inside a word */
-#define NUMBER 30 /* outside a word */
-#define M  10
 
-int sqrt_02(int question);
-
-
-int main()
+ 
+double factorial(unsigned int i)
 {
-    int c, nb,nt,nl;
-    nb=nt=nl=0;
-    while(c=getchar()!=EOF){ // EOF(end of file) 文件结束符 ctrl+Z,回车
-        if(c == '\0'){
-            ++nb;
-        }
-        if(c=='\t'){
-            ++nt;
-        }
-        if(c == '\n'){
-            ++nl;
-        }
-    }
-    printf("%d,%d,%d\n",nb,nt,nl);
-     
+   if(i <= 1)
+   {
+      return 1;
+   }
+   return i * factorial(i - 1);
+}
+int  main()
+{
+    int i = 5;
+    printf(" The factorial of %d is %f\n", i, factorial(i));
+
     system("pause");
-} 
+    return 0;
+}
 
 
--- static.c
+++ static.c
@@ -29,3 +29,46 @@ void func1(void)
 
 
 /************************************************/
+/* 统计输入的空格，换行和制表符*/
+/************************************************/
+int main()
+{
+    int c, nb,nt,nl;
+    nb=nt=nl=0;
+    while(c=getchar()!=EOF){ // EOF(end of file) 文件结束符 ctrl+Z,回车
+        if(c == '\0'){
+            ++nb;
+        }
+        if(c=='\t'){
+            ++nt;
+        }
+        if(c == '\n'){
+            ++nl;
+        }
+    }
+    printf("%d,%d,%d\n",nb,nt,nl);
+     
+    system("pause");
+} 
+
+
+/************************************************/
+/*利用函数地递归来实现 一个数地阶乘 !5=!4*5=!3*4*5 */
+/************************************************/
+
+double factorial(unsigned int i)
+{
+   if(i <= 1)
+   {
+      return 1;
+   }
+   return i * factorial(i - 1);
+}
+int  main()
+{
+    int i = 5;
+    printf(" The factorial of %d is %f\n", i, factorial(i));
+
+    system("pause");
+    return 0;
+}
